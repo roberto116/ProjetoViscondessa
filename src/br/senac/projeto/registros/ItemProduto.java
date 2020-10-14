5eeae998eb11c6f8676e70fb07ddf9c39d88fc87
lenovo <lenovo@LAPTOP-TSNSMQ9L>
2020-10-13 21:13:47 -0300

    Commit passo 1, salvar e pesquisar. Pesquisar não está buscando gênero.

---
+++ src/br/senac/projeto/registros/ItemProduto.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.projeto.registros;
+
+/**
+ *
+ * @author lenovo
+ */
+public class ItemProduto {
+    public int id;
+    public String jogo;
+    public String genero;
+    
+    public String getJogo(){
+    return jogo;
+}
+    public String getGenero(){
+    return genero;
+}
+    
+}
--- src/br/senac/projeto/telas/TelaPrincipalController.java
+++ src/br/senac/projeto/telas/TelaPrincipalController.java
@@ -5,9 +5,20 @@
  */
 package br.senac.projeto.telas;
 
+import br.senac.projeto.registros.ItemProduto;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  * FXML Controller class
@@ -16,12 +27,55 @@ import javafx.fxml.Initializable;
  */
 public class TelaPrincipalController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private TextField tfJogo;
+    @FXML
+    private TableView<ItemProduto> tabelaProduto;
+    @FXML
+    private TableColumn<ItemProduto, String> colunaJogos;
+    @FXML
+    private TableColumn<ItemProduto, String> colunaGenero;
+    @FXML
+    private TextField tfGenero;
+
+    //variaveis globais
+    List <ItemProduto>listaProduto = new ArrayList();
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        colunaJogos.setCellValueFactory(new PropertyValueFactory("jogo"));
+         colunaGenero.setCellValueFactory(new PropertyValueFactory("genero"));
+        
     }    
+
+    @FXML
+    private void salvar(ActionEvent event) {
+        ItemProduto item = new ItemProduto();
+        
+        item.jogo = tfJogo.getText();
+        item.jogo = tfGenero.getText();
+        
+        listaProduto.add(item);
+    }
+
+    @FXML
+    private void editar(ActionEvent event) {
+    }
+
+    @FXML
+    private void pesquisar(ActionEvent event) {
+        tabelaProduto.setItems(FXCollections.observableArrayList(listaProduto));
+    }
+
+    @FXML
+    private void excluir(ActionEvent event) {
+    }
+
+    private static class ItemAgenda extends ItemProduto {
+
+        public ItemAgenda() {
+        }
+    }
     
 }
